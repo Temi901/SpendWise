4eeb60571e36978e7248bb4bf7cb92100f8b3981
Temi901 <jejetemmy8@gmail.com>
2026-07-22 11:02:54 +0100

    Add password reset feature with email

--- spendwise/settings.py
+++ spendwise/settings.py
@@ -136,3 +136,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tracker', 'static'),
 ]
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('jejetemmy8@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('uvnp uvol ygqe jxla')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
